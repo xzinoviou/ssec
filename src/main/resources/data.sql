21d3278adc54b97e0fdc4221e0ccbb6ea049c494
xzinoviou <xzinoviou@gmail.com>
2020-04-26 19:34:22 +0300

    added scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@ INSERT INTO authorities(username,authority) values('user','ROLE_USER');
 
 INSERT INTO users(username,password,enabled) values('admin','admin',true);
 INSERT INTO authorities(username,authority) values('admin','ROLE_ADMIN');
+
+insert into role(id,role_type) values(1,'ADMIN'),(2,'USER'),(3,'MANAGER'),(4,'DEV')
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,27 @@
-create table users(
-	username varchar_ignorecase(50) not null primary key,
-	password varchar_ignorecase(50) not null,
-	enabled boolean not null
-);
+app_user, CREATE TABLE `app_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `enabled` bit(1) NOT NULL,
+  `expired` bit(1) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
-create table authorities (
-	username varchar_ignorecase(50) not null,
-	authority varchar_ignorecase(50) not null,
-	constraint fk_authorities_users foreign key(username) references users(username)
-);
 
-create unique index ix_auth_username on authorities (username,authority);
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `app_user_roles` (
+  `app_users_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`app_users_id`,`role_id`),
+  KEY `FK12tauuariiusdbesvpbww106k` (`role_id`),
+  CONSTRAINT `FK12tauuariiusdbesvpbww106k` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `FKd58qlepf9efgg7iln4mofftvl` FOREIGN KEY (`app_users_id`) REFERENCES `app_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;

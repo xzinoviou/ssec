5ac7c484711aad9519d121b3e13fef76465bcfc7
xzinoviou <xzinoviou@gmail.com>
2020-09-05 00:32:29 +0300

    Refactored JpaConfig

--- src/main/java/com/xzinoviou/ssec/config/JpaConfig.java
+++ src/main/java/com/xzinoviou/ssec/config/JpaConfig.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.xzinoviou.ssec"})
-public class AppConfig {
+public class JpaConfig {
 }
